055ad73d1c61f86cebfa7d45313f689e0993745c
Nelson A Caudill <nelson.a.caudill@gmail.com>
2019-11-09 20:13:11 +0000

    Done.

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,13 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |memo, key|
+    if memo == "Freddy Mercury"
+      key.each do |key, value|
+        if key == :favorite_ice_cream_flavors
+          value.delete_if { |value| value == "strawberry" }
+        end
+      end
+    end
+  end
 end
